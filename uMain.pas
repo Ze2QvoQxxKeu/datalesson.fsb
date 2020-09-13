6e3f961b106f55c9d354d110ffc23f7de326a3b6
Ze2QvoQxxKeu <17485135+Ze2QvoQxxKeu@users.noreply.github.com>
2020-09-13 19:30:17 +0400

    Новый урок
    Новый урок: Искусственный интеллект и машинное обучение

--- uMain.pas
+++ uMain.pas
@@ -79,7 +79,7 @@ procedure TfMain.ПолучитьClick(Sender: TObject);
 
 const
   ЖЕРТВЫ: array[0..2] of string = ('student', 'teacher', 'parent');
-  УРОКИ: array[0..3] of string = ('mail', 'cdw', 'da', 'fs');
+  УРОКИ: array[0..4] of string = ('mail', 'cdw', 'da', 'fs', 'sber');
 var
   SS: TStringStream;
   MS: TMemoryStream;
